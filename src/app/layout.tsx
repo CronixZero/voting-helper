1189797b71394d009b853a85279fa8ea2a717540
CronixZero <cronixfree@gmail.com>
2023-12-13 14:41:59 +0100

    feat: Add Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({subsets: ['latin']})
 export const metadata: Metadata = {
   title: 'Wahlhelfer',
   description: 'Hilfe bei den Wahlen',
+  icons: {
+    icon: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({

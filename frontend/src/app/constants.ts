36cfad5301bbba6fe86c81aa4a1e08b60d1e23d4
CronixZero <cronixfree@gmail.com>
2023-12-24 00:41:41 +0100

    fix: Use NODE_ENV EnvVariable instead of something custom

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,8 +1,8 @@
 const prod_base_url = 'votes.mikka.systems';
 const dev_base_url = 'localhost:8080';
-export const BASE_URL = process.env.PRODUCTION === 'true'
+export const BASE_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
     ? `https://${prod_base_url}`
     : `http://${dev_base_url}`;
-export const WEBSOCKET_URL = process.env.PRODUCTION === 'true'
+export const WEBSOCKET_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
     ? `wss://${prod_base_url}/ws`
     : `ws://${dev_base_url}/ws`;

c014374cd79876df1755e95cc7366199ce4cecec
CronixZero <cronixfree@gmail.com>
2023-12-24 00:21:53 +0100

    fix: Fix ExtensionAppendInterceptor

--- backend/src/main/java/xyz/cronixzero/votinghelper/web/ExtensionAppendInterceptor.java
+++ backend/src/main/java/xyz/cronixzero/votinghelper/web/ExtensionAppendInterceptor.java
@@ -7,9 +7,11 @@ public class ExtensionAppendInterceptor extends PathForwardHandlerInterceptor {
 
   @Override
   protected String provideAlternative(String path) {
-    if (path.startsWith("/api")
+    if (path.equals("/")
+        || path.startsWith("/api")
         || path.startsWith("/actuator")
-        || path.startsWith("/ws")) {
+        || path.startsWith("/ws")
+        || path.contains(".")) {
       return null;
     }
 

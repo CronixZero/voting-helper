e60e03e6088e94baaa5778ae2cf68d737a3f5e57
CronixZero <cronixfree@gmail.com>
2024-03-16 13:14:35 +0100

    fix: Change record type to Integer to support the Json Transmission of Votes

--- backend/src/main/java/xyz/cronixzero/votinghelper/models/Vote.java
+++ backend/src/main/java/xyz/cronixzero/votinghelper/models/Vote.java
@@ -2,6 +2,6 @@ package xyz.cronixzero.votinghelper.models;
 
 import java.util.UUID;
 
-public record Vote(UUID ballotId, UUID candidateId, int rating) {
+public record Vote(UUID ballotId, UUID candidateId, Integer rating) {
 
 }

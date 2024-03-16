425b8bfedb367d631d9ddf890f88e2e109336003
CronixZero <cronixfree@gmail.com>
2024-03-16 13:57:03 +0100

    fix: Another try at using the relative path

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,9 +1,10 @@
-const prod_base_url = window && window?.location?.hostname ? window.location.hostname : "";
-const dev_base_url = 'localhost:8080';
+const isProd = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined;
+const prodBaseUrl = isProd ? window.location.hostname : "";
+const devBaseUrl = 'localhost:8080';
 
-export const BASE_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
-    ? `https://${prod_base_url}` // use relative path in production
-    : `http://${dev_base_url}`;
-export const WEBSOCKET_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
-    ? `wss://${prod_base_url}/ws`
-    : `ws://${dev_base_url}/ws`;
+export const BASE_URL = isProd
+    ? `https://${prodBaseUrl}` // use relative path in production
+    : `http://${devBaseUrl}`;
+export const WEBSOCKET_URL = isProd
+    ? `wss://${prodBaseUrl}/ws`
+    : `ws://${devBaseUrl}/ws`;

5bbf0f6c8f900f240027ce6a5456ac6e14b07764
CronixZero <cronixfree@gmail.com>
2024-03-16 13:34:39 +0100

    fix: Another try at using the relative path

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,7 +1,6 @@
-"use client"
-
-const prod_base_url = window.location.hostname;
+const prod_base_url = window?.location?.hostname ? window.location.hostname : "";
 const dev_base_url = 'localhost:8080';
+
 export const BASE_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
     ? `https://${prod_base_url}` // use relative path in production
     : `http://${dev_base_url}`;

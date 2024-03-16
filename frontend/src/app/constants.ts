5c63735530fa69867ebf19d74cd8b4443e78a79f
CronixZero <cronixfree@gmail.com>
2024-03-16 14:12:55 +0100

    fix: Another try at using the relative path

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,10 +1,29 @@
-const isProd = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined;
-const prodBaseUrl = isProd ? window.location.hostname : "";
+const isProd = process.env.TARGET_ENV === 'production';
+const isStaging = process.env.TARGET_ENV === 'staging';
+
+const prodBaseUrl = "votes.mikka.systems";
+const stagingBaseUrl = "votes-test.mikka.systems"
 const devBaseUrl = 'localhost:8080';
 
-export const BASE_URL = isProd
-    ? `https://${prodBaseUrl}` // use relative path in production
-    : `http://${devBaseUrl}`;
-export const WEBSOCKET_URL = isProd
-    ? `wss://${prodBaseUrl}/ws`
-    : `ws://${devBaseUrl}/ws`;
+export const BASE_URL = getBaseUrl();
+export const WEBSOCKET_URL = getWebsocketUrl();
+
+function getBaseUrl() {
+  if (isProd) {
+    return `https://${prodBaseUrl}`;
+  } else if (isStaging) {
+    return `https://${stagingBaseUrl}`;
+  } else {
+    return `http://${devBaseUrl}`;
+  }
+}
+
+function getWebsocketUrl() {
+  if (isProd) {
+    return `wss://${prodBaseUrl}/ws`;
+  } else if (isStaging) {
+    return `wss://${stagingBaseUrl}/ws`;
+  } else {
+    return `ws://${devBaseUrl}/ws`;
+  }
+}

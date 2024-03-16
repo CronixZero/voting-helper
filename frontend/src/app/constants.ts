fbb15c4d9af1b3ba29f6540733f83267e331358f
CronixZero <cronixfree@gmail.com>
2024-03-16 13:06:27 +0100

    feat: Use relative path in prod

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,7 +1,7 @@
-const prod_base_url = 'votes.mikka.systems';
+const prod_base_url = window.location.hostname;
 const dev_base_url = 'localhost:8080';
 export const BASE_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
-    ? `` // use relative path in production
+    ? `https://${prod_base_url}` // use relative path in production
     : `http://${dev_base_url}`;
 export const WEBSOCKET_URL = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined
     ? `wss://${prod_base_url}/ws`
